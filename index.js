341c96049e69ce3671c306aab296bf149c0eb955
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-20 12:46:04 +0200

    Init

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// Create an empty scene
+const scene = new THREE.Scene();
+
+// Create a basic perspective camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+// Create a renderer with Antialiasing
+const renderer = new THREE.WebGLRenderer({antialias:true});
+
+// Configure renderer clear color
+renderer.setClearColor("#000000");
+
+// Configure renderer size
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+// Append Renderer to DOM
+document.body.appendChild( renderer.domElement );
+
+// Create a Cube Mesh with basic material
+const geometry = new THREE.BoxGeometry( 2, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: "#A33F81" } );
+const cube = new THREE.Mesh( geometry, material );
+
+// Add cube to Scene
+scene.add( cube );
+
+// Set the initial camera position and target
+camera.position.set(0, 0, 5); // Set the initial camera position (e.g., 5 units along the Z-axis)
+camera.lookAt(0, 0, 0); // Set the point the camera will look at initially (e.g., the origin)
+
+// Render Loop
+var render = function () {
+  requestAnimationFrame( render );
+
+ cube.rotation.x += 0.01;
+ cube.rotation.y += 0.01;
+ 
+  // Render the scene
+  renderer.render(scene, camera);
+  
+};
+
+render();
