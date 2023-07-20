fe0d85aef9c8f0ffec6b6ced90c2d55df654bd03
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-20 13:16:37 +0200

    Dodanie ręcznej kontroli

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+
 
 // Create an empty scene
 const scene = new THREE.Scene();
@@ -28,16 +31,18 @@ scene.add( cube );
 // Set the initial camera position and target
 camera.position.set(0, 0, 5); // Set the initial camera position (e.g., 5 units along the Z-axis)
 camera.lookAt(0, 0, 0); // Set the point the camera will look at initially (e.g., the origin)
+const control = new OrbitControls(camera, renderer.domElement);
 
 // Render Loop
 var render = function () {
   requestAnimationFrame( render );
 
- cube.rotation.x += 0.01;
- cube.rotation.y += 0.01;
+ //cube.rotation.x += 0.01;
+ //cube.rotation.y += 0.01;
  
   // Render the scene
   renderer.render(scene, camera);
+  control.update();
   
 };
 
